a7167c0df1245277e15ed4037dbdc1e8e74d5ce8
Wellington Ribeiro <60943819+wellingtonvx@users.noreply.github.com>
2022-07-27 19:42:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function redirect() {
+  window.location.href = "https://sonuscomplete24.com/story.php?aff=andfelipe?subid=&aff_id=&subid2="
+}
+
+$('#redirect').on()
+
+document.querySelector('#redirect').addEventListener('click', redirect)
